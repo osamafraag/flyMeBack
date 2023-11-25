aa13fc76c0d962c30eb64d47ea97d8300903491f
osamafraag <osamafraag19@gmail.com>
2023-11-25 16:16:21 +0200

    addsettings

--- flyMe/urls.py
+++ flyMe/urls.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
+    path('accounts/',include('accounts.urls')),
+    path('countries/',include('countries.urls')),
     path('flights/',include('flights.urls')),
-]
+
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 urlpatterns += staticfiles_urlpatterns()
+
